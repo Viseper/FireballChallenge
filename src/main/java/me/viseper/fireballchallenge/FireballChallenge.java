3e6d69722709d0b6c1ea1a7bdd5b921deef4fa7b
Viseper <Viseper@protonmail.com>
2022-03-07 20:10:34 -0700

    Meow

---
+++ src/main/java/me/viseper/fireballchallenge/FireballChallenge.java
@@ -0,0 +1,55 @@
+package me.viseper.fireballchallenge;
+
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Fireball;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.util.Vector;
+
+public final class FireballChallenge extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+    @EventHandler
+    public void onPlayerClick(PlayerInteractEvent event) {
+        Player player = event.getPlayer();
+    }
+
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent blockBreakEvent) {
+        Player player = blockBreakEvent.getPlayer();
+        if(player != null) {
+            blockBreakEvent.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent playerInteractEvent) {
+        Player player = playerInteractEvent.getPlayer();
+        World world = player.getWorld();
+        if (playerInteractEvent.getAction().isLeftClick()) {
+            int explosivePower = (int)Math.floor(Math.random() * 3) + 1;
+            Fireball fireball = (Fireball) world.spawnEntity(new Location(world, player.getLocation().getX(),player.getLocation().getY() + 1, player.getLocation().getZ()), EntityType.FIREBALL);
+            fireball.setYield(explosivePower);
+            fireball.setVelocity(player.getLocation().getDirection());
+            fireball.setDirection(player.getLocation().getDirection());
+        }
+    }
+
+    }
