31e7d56dd7db6707b74a022617c3533fdbe5c21e
Viseper <Viseper@protonmail.com>
2022-03-07 20:28:57 -0700

    Increased fireball range. Added a random chance for max level fireball to spawn.

--- src/main/java/me/viseper/fireballchallenge/FireballChallenge.java
+++ src/main/java/me/viseper/fireballchallenge/FireballChallenge.java
@@ -8,6 +8,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.util.Vector;
@@ -44,11 +45,23 @@ public final class FireballChallenge extends JavaPlugin implements Listener {
         Player player = playerInteractEvent.getPlayer();
         World world = player.getWorld();
         if (playerInteractEvent.getAction().isLeftClick()) {
-            int explosivePower = (int)Math.floor(Math.random() * 3) + 1;
+            int explosivePower = (int)Math.floor(Math.random() * 5) + 1;
+            if(Math.floor(Math.random() * 10) == 2.0 && Math.floor(Math.random() * 10) == 2.0) {
+                explosivePower = 127;
+            }
             Fireball fireball = (Fireball) world.spawnEntity(new Location(world, player.getLocation().getX(),player.getLocation().getY() + 1, player.getLocation().getZ()), EntityType.FIREBALL);
             fireball.setYield(explosivePower);
             fireball.setVelocity(player.getLocation().getDirection());
             fireball.setDirection(player.getLocation().getDirection());
+        }
+    }
+
+    @EventHandler
+    public void onPlayerDamage(EntityDamageByEntityEvent damage) {
+        Fireball fireball = (Fireball) damage.getDamager();
+        Player player = (Player) damage.getEntity();
+        if (player != null && fireball != null) {
+            damage.setDamage(0);
         }
     }
 
